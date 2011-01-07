a4de5e189496310ee18a06072a320afb1dd0757c
David Hrachovy <davidhrachovy@atlas.cz>
2011-01-07 20:50:12 +0100

    fractal: vertex normals

--- src/impl/fractallandscape.h
+++ src/impl/fractallandscape.h
@@ -2,8 +2,10 @@
 #include "../core/array2.h"
 class FractalLandscape
 {
+public:
 	Array2<float> heights;
-	Array2<Vector> normals;
+	Array2<Vector> squareNormals;
+	Array2<Vector> vertexNormals;
 	float width, height;
 	uint number_of_squares_in_one_axis;
 	uint number_of_vertices_in_one_axis;
@@ -26,6 +28,8 @@ class FractalLandscape
 		generateVertexNormals();
 		generateTriangles();
 	}
+	//Adds all triangles contained in this object to a scene
+	void addReferencesToScene(std::vector<Primitive*> &_scene) const;
 protected:
 	void resetHeights(float h)
 	{
@@ -46,13 +50,40 @@ protected:
 				Vector vy(0,
 					 one_square_width,
 					 heights(x,y + 1) - heights(x,y));
-				normals(x,y) = vx % vy;
+				squareNormals(x,y) = ~(vx % vy);
 			}
 	}
 	
-	void generateVertexNormals();
+	void generateVertexNormals()
+	{
+		for(uint y = 0; y < number_of_vertices_in_one_axis; y++) 
+			for(uint x = 0; x < number_of_vertices_in_one_axis; x++) {
+				Vector n(0,0,0);
+				//left-up
+				if(x == 0 || y == number_of_squares_in_one_axis)
+					n = n + Vector(0,0,1);
+				else
+					n = n + squareNormals(x - 1, y);
+				//right-up
+				if(x == number_of_squares_in_one_axis || y == number_of_squares_in_one_axis)
+					n = n + Vector(0,0,1);
+				else
+					n = n + squareNormals(x, y);				
+				//right-down
+				if(x == number_of_squares_in_one_axis || y == 0)
+					n = n + Vector(0,0,1);
+				else
+					n = n + squareNormals(x, y - 1);			
+				//left-down
+				if(x == 0 || y == 0)
+					n = n + Vector(0,0,1);
+				else
+					n = n + squareNormals(x - 1, y - 1);
+				n = ~n;
+			}		
+	}
 	void generateTriangles();
-	//Adds all triangles contained in this object to a scene
-	void addReferencesToScene(std::vector<Primitive*> &_scene) const;
+	
+
 };
 
