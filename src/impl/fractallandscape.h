87652c3ba5aeeec862a64bdf27d4ac834e9612a7
David Hrachovy <davidhrachovy@atlas.cz>
2011-01-07 17:46:39 +0100

    Fractal: constructor

--- src/impl/fractallandscape.h
+++ src/impl/fractallandscape.h
@@ -2,8 +2,18 @@
 class FractalLandscape
 {
 	std::vector<float> heights;
-	FractalLandscape(Point *a, Point *b, uint iterations) {
-		
+	float width, height;
+	uint number_of_squares_in_one_axis;
+	uint number_of_vertices_in_one_axis;
+	Point corner;
+	
+	FractalLandscape(Point &a, Point &b, uint iterations) {
+		corner = Point(std::min(a[0], b[0]), std::min(a[1], b[1]), a[2]);
+		width = abs(a[0] - b[0]);
+		height = abs(a[1] - b[1]);
+		number_of_squares_in_one_axis = pow(2, iterations);
+		number_of_vertices_in_one_axis = number_of_squares_in_one_axis + 1;
+		heights.resize(number_of_squares_in_one_axis * number_of_squares_in_one_axis);
 	}
 
 	//Adds all triangles contained in this object to a scene
