71d0138bf88154bfeb4f7499ad8fabf4c3f35a2c
David Hrachovy <davidhrachovy@atlas.cz>
2011-01-12 22:26:19 +0100

    perlin: make it render like original perlin

--- src/core/perlin.h
+++ src/core/perlin.h
@@ -13,26 +13,27 @@ public:
 	SmoothNoise(uint _width)
 	{
 		// widht + 4
-		noise = Array2<float>(width + 2);
+		noise = Array2<float>(_width + 4);
 		width = _width;
 		generateRandoms();
 	}
 	void generateRandoms()
 	{
-		for(int y = 0; y < width; y++)
-			for(int x = 0; x < width; x++)
+		for(int y = 0; y < width + 4; y++)
+			for(int x = 0; x < width + 4; x++) {
 				noise(x, y) = normalizedRand();
+			}
 	}
 
 	float normalizedRand() 
 	{
-		return rand() / (float)RAND_MAX;	
+		return (((rand() / (float)RAND_MAX)) * 2) - 1.0;	
 	}
 	
 	float smoothedSample(uint x, uint y)
 	{
-		x += 1;
-		y += 1;
+		x += 2;
+		y += 2;
 		float corners = (noise(x-1,y-1) + noise(x+1,y+1) 
 			+ noise(x - 1,y + 1) + noise(x + 1, y- 1)) / 16.0f;
 		float sides = (noise(x-1,y) + noise(x,y-1) 
@@ -51,13 +52,19 @@ public:
 	std::vector<SmoothNoise> noises;
 	int n_octaves;
 	uint width;
+	float c;
 	//constructor
-	Perlin(uint _width, float persistence) 
+	Perlin(uint _width, float _persistence, int _n_octaves) 
 	{
 		width = _width;
-		int n_octaves = 3;
-		for(int i = 1; i <= n_octaves; i++)
-			noises.push_back(SmoothNoise(i * width));
+		n_octaves = _n_octaves;
+		persistence = _persistence;
+		c = 5;
+		uint w = width;
+		for(int i = 1; i <= n_octaves; i++) {
+			noises.push_back(SmoothNoise(w));
+			w *= 2;
+		}
 		
 	}
 	float linearInterpolation(float fx, float fy, float fraction) 
@@ -72,18 +79,30 @@ public:
 		float fractiony = fy - y;
 		float l1 = linearInterpolation(noises[noise_i].smoothedSample(x, y), noises[noise_i].smoothedSample(x + 1, y), fractionx);
 		float l2 = linearInterpolation(noises[noise_i].smoothedSample(x, y + 1), noises[noise_i].smoothedSample(x + 1, y + 1), fractionx);
-		return linearInterpolation(l1, l2, fractiony);	
+		float li = linearInterpolation(l1, l2, fractiony);	
+		return li;
+	}
+	float noise(float x, float y, float frequency, float amplitude, int noise_i) 
+	{
+		return (interpolatedNoise(x * frequency, y * frequency, noise_i)) * amplitude;
+	}
+	float customize(float x, float y, float total) 
+	{
+		//return 0.5 * (1.0 + sin(x + c*total));
+		//return sin(x +  total);
+		return (total + 1.0) / 2.0;
 	}
 	float sample(float x, float y) 
 	{
 		float frequency = 1;
-		float amplitude = persistence;
+		float amplitude = 1;
 		float total = 0.0f;
 		for(int i = 0; i < n_octaves; i ++) {
-			total += interpolatedNoise(x * frequency ,y * frequency, i) * amplitude;
+			total += noise(x, y, frequency, amplitude, i);
 			frequency *= 2;
-			persistence *= persistence;
+			amplitude *= persistence;
 		}
+		return customize(x, y, total);
 	}
 };
 #endif //__PERLIN_INCLUDED
